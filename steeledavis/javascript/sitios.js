f46a593dcf0c0016e90a2b8475df0f44fbd3c29b
Caroluk <clukoski@frd.utn.edu.ar>
2021-06-23 17:24:52 -0300

    jQuery, intente hacer funcional el buscador y no pude. Modificaciones en css respecto al input de busqueda

--- steeledavis/javascript/sitios.js
+++ steeledavis/javascript/sitios.js
@@ -44,3 +44,108 @@ $('aside a').on('click', function(evento) {
     }
     
     )
+
+// dando vida al buscador 
+
+
+var buscador = $("#input-buscar");
+var listaResultados = $("#busquedas");
+var resultados = $(".resultado");
+
+var resultados = ["DRONES", "CONTACTO", "MAIL", "INFORMACION", "CONSEJOS"]
+
+if (buscador.indexOf = resultados[0] || resultados[1] || resultados[2] || resultados[3] || resultados[4] || resultados[5] ){
+
+    console.log("bien")
+ 
+} else {
+
+    console.log("mal")
+}
+
+
+
+
+/*function buscador_interno(){
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+    for (i = 0; i < li.length; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
+                }else{
+                    li[i].style.display = "none";
+                }
+
+        }
+
+} 
+
+
+var buscador = $("#input-buscar");
+var listaResultados = $("#busquedas");
+var resultados = $(".resultado");
+
+$("#input-buscar").keyup(buscador_interno);
+
+function buscador_interno() {
+    filtro = buscador.value.toUpeerCase();
+    li = listaResultados.$("li"); 
+
+    for(i = 0;i < li.length; i ++){
+        a = li[i].$("a"[0]);
+        textValor = a.textContent || a.innerText;
+
+        if(textValor.toUpeerCase().indexOf(filtro) > -1){
+
+            li[i].style.display = "block";
+
+            if(buscador.value === "") {
+                listaResultados.style.display ="none";
+            } 
+        } else { 
+
+            li[i].style.display="none";
+
+        }
+
+    }
+
+}
+
+
+
+
+
+var buscador = $("#input-buscar");
+var listaResultados = $("#busquedas");
+var resultados = $(".resultado");
+
+for(i = 0;i < li.length; i ++) {
+
+    if (buscador.value = resultados[0] || resultados[1]) {
+
+        var $link = $(evento.target)
+        var url = $link.prop('href')
+    
+        $.get(url,function(contenido) {
+            $('main').html(contenido)
+        })
+
+
+}
+
+}
+*/
+

3091112ef9de6e62c3381339a2ec58256440d54c
Caroluk <clukoski@frd.utn.edu.ar>
2021-06-25 18:04:30 -0300

    seguimos con el input search

--- steeledavis/javascript/sitios.js
+++ steeledavis/javascript/sitios.js
@@ -50,120 +50,14 @@ $("#input-search").keyup(function () {
 
     _this = this;
 
+    
     $.each($("tbody tr"), function () {
 
-        if ($(this).text().toUpperCase().indexOf($(_this).val().toUpperCase()) === -1)
+        if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
             $(this).hide();
-        else
+            $(this).css('color: blue');
+         } else {
             $(this).show();
-    });
+
+    }});
 });
-
-/* 
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario)
-
-var buscador = $('#table').DataTable();
-
-$('#input-search').keyup(function() {
-
-    buscador.search($(this).val()).draw();
-
-    if ($('#input-search').val() == "") {
-
-        $('.div-busquedas').fadeOut;
-
-    } else {
-
-        $('.div-busquedas').fadeIn;
-
-    }
-
-})
-
-
-
-
-/*function buscador_interno(){
-    filter = inputSearch.value.toUpperCase();
-    li = box_search.getElementsByTagName("li");
-
-    //Recorriendo elementos a filtrar mediante los "li"
-    for (i = 0; i < li.length; i++){
-
-        a = li[i].getElementsByTagName("a")[0];
-        textValue = a.textContent || a.innerText;
-
-        if(textValue.toUpperCase().indexOf(filter) > -1){
-
-            li[i].style.display = "";
-            box_search.style.display = "block";
-
-            if (inputSearch.value === ""){
-                box_search.style.display = "none";
-            }
-                }else{
-                    li[i].style.display = "none";
-                }
-
-        }
-
-} 
-
-
-var buscador = $("#input-buscar");
-var listaResultados = $("#busquedas");
-var resultados = $(".resultado");
-
-$("#input-buscar").keyup(buscador_interno);
-
-function buscador_interno() {
-    filtro = buscador.value.toUpeerCase();
-    li = listaResultados.$("li"); 
-
-    for(i = 0;i < li.length; i ++){
-        a = li[i].$("a"[0]);
-        textValor = a.textContent || a.innerText;
-
-        if(textValor.toUpeerCase().indexOf(filtro) > -1){
-
-            li[i].style.display = "block";
-
-            if(buscador.value === "") {
-                listaResultados.style.display ="none";
-            } 
-        } else { 
-
-            li[i].style.display="none";
-
-        }
-
-    }
-
-}
-
-
-
-
-
-var buscador = $("#input-buscar");
-var listaResultados = $("#busquedas");
-var resultados = $(".resultado");
-
-for(i = 0;i < li.length; i ++) {
-
-    if (buscador.value = resultados[0] || resultados[1]) {
-
-        var $link = $(evento.target)
-        var url = $link.prop('href')
-    
-        $.get(url,function(contenido) {
-            $('main').html(contenido)
-        })
-
-
-}
-
-}
-*/
-

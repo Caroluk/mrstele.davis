6a6f1ba589d75461e15ec62276c70fdd0822d31d
Caroluk <clukoski@frd.utn.edu.ar>
2021-06-25 23:43:34 -0300

    intento de alert en el button-search sale mal

--- steeledavis/javascript/sitios.js
+++ steeledavis/javascript/sitios.js
@@ -35,29 +35,61 @@ $('a').on('click', function(evento) {
     
 })
 
-$('textarea').on('keyup', function(clear) {
-
-    console.log('hola')
-
-})
-
-
-
-
 // dando vida al buscador 
 
 $("#input-search").keyup(function () {
 
     _this = this;
 
-    
     $.each($("tbody tr"), function () {
 
-        if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
+
+        if ($(this).text().toLowerCase().indexOf($(_this).val()) == -1) {
+
             $(this).hide();
-            $(this).css('color: blue');
+
          } else {
-            $(this).show();
+            $(this).show(); 
 
     }});
+
 });
+
+
+$('#button-search').on('click', function() {
+
+    var $text = $('#table td').textContent;
+
+    if ($('#input-search').text != $text ) {
+
+        alert('Parece que tu busqueda no coincide con nuestros resultados, prueba con algunos de los resultados sugeridos!')
+        
+    } else {
+
+        alert('no funciona')
+    }
+
+})
+
+/* 
+
+
+-----------------
+
+
+            if($('#input-search').getText() != $("#table").tableDate){
+                console.log("hola");
+            }
+             else{
+                console.log("chau");
+               }    
+
+-------------------------------
+
+        if ($(this) != $(this).hide || $(this).show) {
+
+            $('main').load("error.html")
+            
+        } else {
+            console.log('hola')
+     */ 

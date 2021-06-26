60efbe0a5c1f73baaf7ba97dc31d791c2257ddbf
Caroluk <clukoski@frd.utn.edu.ar>
2021-06-26 16:29:35 -0300

    el input search se parece mas a un buscador. tratando de darle funcionalidad al buton search y que se limpien los campos del mail automaticamente en cuando este se envia

---
+++ steeledavis/javascript/buscador.js
@@ -0,0 +1,52 @@
+// dando vida al buscador 
+
+$("#input-search").keyup(function () {
+
+    _this = this;
+
+        $.each($("tbody tr"), function () {
+
+    
+            if ($(this).text().toLowerCase().indexOf($(_this).val()) == -1) {
+    
+                $(this).hide();
+    
+             } else {
+                $(this).show();
+                $('.div-tabla').css({
+                    "height": "160px"})
+    
+        }});
+
+});
+
+/*
+$('#button-search').on('click', function() {
+
+    var $text = $('#table td').textContent;
+
+    if ($('#input-search').text != $text) {
+
+        alert('Parece que tu busqueda no coincide con nuestros resultados, prueba con algunas de nuestras sugerencias!')
+        
+    } 
+})
+
+*/
+
+$('#button-search').on('click', function() {
+
+    const posibles = ['drones','contacto','mail','informacion','consejos']
+
+    var $text = $('#table td').textContent;
+
+    if ($('#input-search').text == posibles[0] || posibles[1] || posibles[2] || posibles[3] || posibles[4] ) {
+
+        alert('Parece que tu busqueda no coincide con nuestros resultados, prueba con algunas de nuestras sgugerencias!')
+        
+    } else {
+
+        alert('no funciona')
+    }
+
+})
--- steeledavis/javascript/sitios.js
+++ steeledavis/javascript/sitios.js
@@ -35,61 +35,6 @@ $('a').on('click', function(evento) {
     
 })
 
-// dando vida al buscador 
-
-$("#input-search").keyup(function () {
-
-    _this = this;
-
-    $.each($("tbody tr"), function () {
 
 
-        if ($(this).text().toLowerCase().indexOf($(_this).val()) == -1) {
 
-            $(this).hide();
-
-         } else {
-            $(this).show(); 
-
-    }});
-
-});
-
-
-$('#button-search').on('click', function() {
-
-    var $text = $('#table td').textContent;
-
-    if ($('#input-search').text != $text ) {
-
-        alert('Parece que tu busqueda no coincide con nuestros resultados, prueba con algunos de los resultados sugeridos!')
-        
-    } else {
-
-        alert('no funciona')
-    }
-
-})
-
-/* 
-
-
------------------
-
-
-            if($('#input-search').getText() != $("#table").tableDate){
-                console.log("hola");
-            }
-             else{
-                console.log("chau");
-               }    
-
--------------------------------
-
-        if ($(this) != $(this).hide || $(this).show) {
-
-            $('main').load("error.html")
-            
-        } else {
-            console.log('hola')
-     */ 
